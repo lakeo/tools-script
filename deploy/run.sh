ca021666a3b7958a09f2ec241a1e330bdd15c424
lanxiaolu <lanxiaolu@meituan.com>
2015-04-16 10:18:16 +0800

    save

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #init global  variables
-JAVA=/usr/local/java/bin/java
+JAVA=$JAVA_HOME
 LOG_ROOT=/opt/logs/
 PORT=8080
+JETTY_HOME=/usr/local/jetty9.1
+JETTY_BASE=$JETTY_HOME
+JETTY_RUN=$JETTY_BASE
+JETTY_USER=www
 $PROJECT=$2
 $WEB_ROOT=/opt/apps/$PROJECT
 $PROJECT_SOURCE_DIR=/home/lxl/$PROJECT
@@ -32,6 +36,36 @@ function __start() {
 }
 #run 
 function __run() {
+    JAVA_OPTIONS="-Xloggc:/opt/log/gc.log 
+    -XX:+PrintGCDetails -XX:+PrintGCDateStamps 
+    -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 
+    -XX:GCLogFileSize=10M -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions 
+    -server 
+    -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:+HeapDumpOnOutOfMemoryError 
+    -XX:HeapDumpPath=/opt/log/oom.log
+    -Djetty.webroot=$WEB_ROOT/webroot
+    -Djetty.logs=$LOG_ROOT
+    -Djetty.context=/
+    "
+
+    JAVA_SIZE="-XX:G1MaxNewSizePercent=50 -XX:PermSize=256m -XX:MaxPermSize=256m -Xss256k -Xms512m -Xmx512m";
+    JAVA_GC="-XX:+DisableExplicitGC -XX:+PrintGCDetails 
+    -XX:+PrintHeapAtGC -XX:+PrintTenuringDistribution 
+    -XX:CMSFullGCsBeforeCompaction=0 -XX:+UseCMSCompactAtFullCollection -XX:CMSInitiatingOccupancyFraction=80
+    -XX:+UseConcMarkSweepGC -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps"
+
+
+    cd $WEB_ROOT/webroot
+    CLASSPATH=WEB-INF/classes
+    for i in WEB-INF/lib/*
+    do
+        CLASSPATH=$CLASSPATH:$i
+    done
+
+    export CLASSPATH
+
+    EXEC="exec $JAVA_OPTIONS $JAVA_SIZE $JAVA_GC"
+
 }
 
 #stop
