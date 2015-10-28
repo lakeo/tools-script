b43167f4374fb19e8114661df5e02ec04fa81349
lanxiaolu <lanxiaolu@meituan.com>
2015-10-28 21:11:13 +0800

    add js

--- chrome/chrome/my.js
+++ chrome/chrome/my.js
@@ -14,5 +14,11 @@ function initListener() {
             }
         });
 }
-console.log('before init')
+//console.log($.fn.jquery)
+function getUserList() {
+jQuery('.member_li').each(function(index){var val=jQuery(this).find('em:first').text();if(val == 'Y'){ jQuery(this).css('border','3px solid red')};})
+jQuery('.member_li').each(function(index){var val=jQuery(this).find('em:first').text();if(val == 'Y'){ jQuery(this).css('border','3px solid red')}; var str = jQuery(this).attr('action-data'); console.log(str.substring(0,str.indexOf('&')))})
+jQuery('.member_li').each(function(index){var val=jQuery(this).find('em:first').text();if(val == 'Y'){ jQuery(this).css('border','3px solid red')}; var str = jQuery(this).attr('action-data'); uid=str.substring(1,str.indexOf('&')); jQuery.ajax({url:'http://weibo.com/aj/f/unfollow?ajwvr=6&__rnd=1446035556178',data:{'uid':uid,refer_flag:'unfollow_all',_t:0}})})
+jQuery('.member_li').each(function(index){var val=jQuery(this).find('em:first').text();if(val == 'Y'){ jQuery(this).css('border','3px solid red')}; var str = jQuery(this).attr('action-data'); uid=str.substring(4,str.indexOf('&')); console.log(uid);jQuery.ajax({url:'http://weibo.com/aj/f/unfollow?ajwvr=6&__rnd=1446035556178',data:{'uid':uid,refer_flag:'unfollow_all',_t:0}})})
+}
 initListener();
