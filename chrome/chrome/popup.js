6799909b5688667ec2836b42f82f5cc866a0e23a
lanxiaolu <lanxiaolu@meituan.com>
2015-10-30 21:29:45 +0800

    change weibo js

--- chrome/chrome/popup.js
+++ chrome/chrome/popup.js
@@ -112,6 +112,13 @@ function initListener() {
     });
   }
 
+  document.getElementById('getAllUserBtn').onclick = function(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {cmd: "getAllAttentionUser"}, function(response) {
+                console.log(response);
+            });
+        });
+    }
   /*
   chrome.runtime.onMessage.addListener(
       function(request, sender, sendResponse) {
