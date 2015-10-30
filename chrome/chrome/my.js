5617ec76b1fd2eb97ab515af699bcc39cc4b2dac
lanxiaolu <lanxiaolu@meituan.com>
2015-10-30 21:50:24 +0800

    complete weibo js

--- chrome/chrome/my.js
+++ chrome/chrome/my.js
@@ -23,14 +23,27 @@ function initListener() {
             }
         });
 }
+
 //console.log($.fn.jquery)
+function beforeClick() {
+    console.log('before click')
+    jQuery('div .W_fl a[action-type="batselect"]')[0].click();
+}
+function afterClick() {
+    jQuery('div .W_fl a[node-type="cancelFollowBtn"]')[0].click()
+    jQuery('div .W_layer_btn a[node-type="ok"]')[0].click()
+}
 function getSinglelUser() {
+    beforeClick();
     console.log('select user')
     jQuery('.member_li').each(function(index){val = jQuery(this).find('em:first').text();if( !val || val == 'Y' ){jQuery(this).click()}  })
+    afterClick();
 }
 
 function getAllUser() {
     console.log('select user')
+    beforeClick();
     jQuery('.member_li').each(function(index){val = jQuery(this).find('em:first').text();if( true ){jQuery(this).click()}  })
+    afterClick();
 }
 initListener();
