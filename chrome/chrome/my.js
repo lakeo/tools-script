998bd1ba858cba2def8b82a9a19f143c547d8114
lanxiaolu <lanxiaolu@meituan.com>
2015-11-04 22:48:40 +0800

    fix chrome

--- chrome/chrome/my.js
+++ chrome/chrome/my.js
@@ -1,29 +1,74 @@
 function initListener() {
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
-            console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-                "from the extension");
-            if (request.cmd == "getSingleAttentionUser"){
-                getSinglelUser();
+
+            if (request.cmd == "cancelSingleAttentionUser"){
+
+                cancelSingleUser();
                 sendResponse(
                     {
                         cmd:'userList',
                         data:'empty'
                     }
                 )
-            }else if (request.cmd == "getAllAttentionUser"){
-                getAllUser();
+            }else if (request.cmd == "cancelAllAttentionUser"){
+
+                cancelAllUser();
                 sendResponse(
                     {
                         cmd:'userList',
                         data:'empty'
                     }
                 )
+            }else if (request.cmd == "payAttendToAll"){
+                checkAlert(request);
+                payAttendToAll();
+                sendResponse(
+                    {
+                        cmd:'userList',
+                        data:'empty'
+                    }
+                )
+            } else if (request.cmd == 'likeAllUser') {
+                likeAll();
             }
         });
 }
 
+function sleepRun(index,list,time,callback){
+    if (index >= list.length) {
+        return;
+    }
+    setTimeout(function(){
+        callback(index,list);
+        sleepRun(index+1, list, time,callback);
+    },time);
+}
+function likeAll() {
+
+    var list = jQuery('a[action-type="fl_like"][title="赞"]');
+    var total = list.length;
+
+    sleepRun(0,list,2500,function(index, array){
+        array[index].click();
+        jQuery('div[node-type="outer"]').css('display','none')
+        jQuery('.W_layer').css('display','none')
+        jQuery('a[action-type="ok"][node-type="ok"]').click()
+        var page = jQuery('a[action-type="feed_list_page"]')
+        if(page.length == 0) {
+            window.scrollTo(0,document.body.scrollHeight);
+        }
+    })
+}
+function checkAlert(request) {
+    if(request.alert) {
+        var secret = prompt(request.msg+"万分抱歉，广告时间：请微信关注“索尔眼镜”。如果不想出现次对话框，请联系微信客服免费获取密码")
+        console.log(secret)
+        if (secret != null) {
+            chrome.storage.sync.set({'soul_secret': secret})
+        }
+    }
+}
 //console.log($.fn.jquery)
 function beforeClick() {
     console.log('before click')
@@ -33,17 +78,21 @@ function afterClick() {
     jQuery('div .W_fl a[node-type="cancelFollowBtn"]')[0].click()
     jQuery('div .W_layer_btn a[node-type="ok"]')[0].click()
 }
-function getSinglelUser() {
+function cancelSingleUser() {
     beforeClick();
-    console.log('select user')
-    jQuery('.member_li').each(function(index){val = jQuery(this).find('em:first').text();if( !val || val == 'Y' ){jQuery(this).click()}  })
+    jQuery('.member_li').each(function(index){val = jQuery(this).find('.statu em:first');console.log(val);if( val.text() == 'Y' ){jQuery(this).click()}})
     afterClick();
 }
 
-function getAllUser() {
+function cancelAllUser() {
     console.log('select user')
     beforeClick();
     jQuery('.member_li').each(function(index){val = jQuery(this).find('em:first').text();if( true ){jQuery(this).click()}  })
     afterClick();
+}
+
+function payAttendToAll() {
+    jQuery('a[action-type="follow"]').each(function(){this.click()})
+    jQuery('a[action-type="cancel"]')[0].click()
 }
 initListener();
--- chrome/chrome/popup.js
+++ chrome/chrome/popup.js
@@ -100,36 +100,90 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
+function checkTime(callback) {
+    chrome.storage.sync.get(['soulPayAttendCnt','soul_secret'], function(value) {
+        var secret=null
+        var val = null
+        var showalert = false;
+        if(typeof value === "undefined") {
+            val = 1
+        }else {
+            secret = value['soul_secret'];
+            val = value['soulPayAttendCnt']
+        }
+        console.log(value)
+        if(typeof val !== 'number') {
+            val = 1
+        }
+        chrome.storage.sync.set({'soulPayAttendCnt':(val+1)})
+
+        if (val % 50 == 0) {
+            showalert = true;
+            console.log('已经使用了五十次该功能，作为友情赞助会打开我们的网站。sorry');
+            var newURL = "http://www.gouwu168.com/";
+            chrome.tabs.create({ url: newURL });
+        }else if (val % 500 == 0) {
+            console.log('已经使用了五百次该功能，作为友情赞助会打开我们的网站。sorry');
+            var newURL = "http://www.gouwu168.com/";
+            chrome.tabs.create({ url: newURL });
+        }
+        if(false) {
+            var secret = prompt("万分抱歉，广告时间：请微信关注“索尔眼镜”。如果不想出现次对话框，请联系微信客服免费获取密码")
+            console.log(secret)
+            if (secret != null) {
+                chrome.storage.sync.set({'soul_secret': secret})
+            }
+        }
+        callback();
+    })
+}
 function initListener() {
   //chrome.tabs.executeScript(null, {file: "jquery-2.1.4.min.js"});
   //chrome.tabs.executeScript(null, {file: "my.js"});
-
-  document.getElementById('getUserBtn').onclick = function(){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {cmd: "getSingleAttentionUser"}, function(response) {
-        console.log(response);
-      });
-    });
+  document.getElementById('url').onclick = function() {
+      var newURL = "http://www.gouwu168.com/weibotool";
+      chrome.tabs.create({ url: newURL });
   }
 
-  document.getElementById('getAllUserBtn').onclick = function(){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {cmd: "getAllAttentionUser"}, function(response) {
-                console.log(response);
-            });
-        });
+  document.getElementById('cancelUserBtn').onclick = function(){
+      checkTime(function(){
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+              chrome.tabs.sendMessage(tabs[0].id, {cmd: "cancelSingleAttentionUser"}, function(response) {
+                  console.log(response);
+              });
+          });
+      })
+  }
+
+  document.getElementById('cancelAllUserBtn').onclick = function(){
+      checkTime(function(){
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+              chrome.tabs.sendMessage(tabs[0].id, {cmd: "cancelAllAttentionUser"}, function(response) {
+                  console.log(response);
+              });
+          });
+      })
     }
-  /*
-  chrome.runtime.onMessage.addListener(
-      function(request, sender, sendResponse) {
-        console.log(sender.tab ?
-        "from a content script:" + sender.tab.url :
-            "from the extension");
-        if (request.cmd == "userList"){
-          console.log(request.data)
-        }
-      });
-      */
+
+    document.getElementById('likeBtn').onclick = function(){
+        checkTime(function(){
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {cmd: "likeAllUser"}, function(response) {
+                    console.log(response);
+                });
+            });
+        })
+    }
+
+    document.getElementById('payAttendToAllBtn').onclick = function(){
+        checkTime(function(){
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {cmd: "payAttendToAll",alert:false, msg:''}, function(response) {
+                    console.log(response);
+                });
+            });
+        })
+    }
 
 }
 
