20307cd61f41a46671b8f25aa58837f19ac23cbc
lanxiaolu <blue.xiaolu@gmail.com>
2015-11-16 08:16:50 +0800

    add auto model

--- chrome/chrome/my.js
+++ chrome/chrome/my.js
@@ -1,7 +1,6 @@
 function initListener() {
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
-
             if (request.cmd == "cancelSingleAttentionUser"){
 
                 cancelSingleUser();
@@ -31,68 +30,47 @@ function initListener() {
                 )
             } else if (request.cmd == 'likeAllUser') {
                 likeAll();
+            } else if (request.cmd == 'openAutoModelBtn') {
+                openAutoModel();
+            } else if (request.cmd == 'closeAutoModelBtn') {
+                closeAutoModel();
             }
         });
 }
 
-function sleepRun(index,list,time,callback){
-    if (index >= list.length) {
+function main() {
+    if(typeof(Storage) === "undefined") {
+        console.log('Sorry! No Web Storage support..');
         return;
     }
+
+    var mainStatus =  localStorage.soul_main_status;
+    if (typeof(mainStatus) === 'undefined') {
+        console.log('soul_main_status is null')
+        return;
+    }
+    console.log('current status is ' + mainStatus);
+
+    //避免错误发生，在300秒之后如果无事情发生，必然会刷新
     setTimeout(function(){
-        callback(index,list);
-        sleepRun(index+1, list, time,callback);
-    },time);
-}
-function likeAll() {
+        if (mainStatus != MANUALLY)
+            window.location.href='http://weibo.com';
+    },305000);
 
-    var list = jQuery('a[action-type="fl_like"][title="赞"]');
-    var total = list.length;
-
-    sleepRun(0,list,2500,function(index, array){
-        array[index].click();
-        jQuery('div[node-type="outer"]').css('display','none')
-        jQuery('.W_layer').css('display','none')
-        jQuery('a[action-type="ok"][node-type="ok"]').click()
-        var page = jQuery('a[action-type="feed_list_page"]')
-        if(page.length == 0) {
-            window.scrollTo(0,document.body.scrollHeight);
+    try{
+        if (mainStatus == MANUALLY) {
+            console.log('do nothing');
+        } else if (mainStatus == AUTO_PAY_ATTENTION_STATUS) {
+            autoPayAttention();
+        } else if (mainStatus == AUTO_CANCELL_ATTENTION_STATUS) {
+            autoCancelAttention();
+        } else if (mainStatus == AUTO_LIKE_ALL_STATUS) {
+            autoLikeAll();
         }
-    })
-}
-function checkAlert(request) {
-    if(request.alert) {
-        var secret = prompt(request.msg+"万分抱歉，广告时间：请微信关注“索尔眼镜”。如果不想出现次对话框，请联系微信客服免费获取密码")
-        console.log(secret)
-        if (secret != null) {
-            chrome.storage.sync.set({'soul_secret': secret})
-        }
+    }catch(err) {
+        console.log(err);
     }
 }
-//console.log($.fn.jquery)
-function beforeClick() {
-    console.log('before click')
-    jQuery('div .W_fl a[action-type="batselect"]')[0].click();
-}
-function afterClick() {
-    jQuery('div .W_fl a[node-type="cancelFollowBtn"]')[0].click()
-    jQuery('div .W_layer_btn a[node-type="ok"]')[0].click()
-}
-function cancelSingleUser() {
-    beforeClick();
-    jQuery('.member_li').each(function(index){val = jQuery(this).find('.statu em:first');console.log(val);if( val.text() == 'Y' ){jQuery(this).click()}})
-    afterClick();
-}
 
-function cancelAllUser() {
-    console.log('select user')
-    beforeClick();
-    jQuery('.member_li').each(function(index){val = jQuery(this).find('em:first').text();if( true ){jQuery(this).click()}  })
-    afterClick();
-}
-
-function payAttendToAll() {
-    jQuery('a[action-type="follow"]').each(function(){this.click()})
-    jQuery('a[action-type="cancel"]')[0].click()
-}
 initListener();
+main();
---
+++ chrome/chrome/tool.js
@@ -0,0 +1,267 @@
+var WEIBO = 'http://weibo.com/'
+var MANUALLY = 'manually';
+var AUTO_PAY_ATTENTION_STATUS = 'auto_pay_attention_status';
+var AUTO_CANCELL_ATTENTION_STATUS = 'auto_cancel_attention_status';
+var AUTO_LIKE_ALL_STATUS = 'auto_like_all_status';
+
+function clearAllStatus() {
+    localStorage.removeItem('auto_pay_attention_status');
+    localStorage.removeItem('auto_pay_attention_status_times');
+    localStorage.removeItem('auto_cancel_attention_status');
+    localStorage.removeItem('auto_cancel_attention_status_times');
+    localStorage.removeItem('auto_cancel_attention_status')
+}
+
+function getCurrentUserId() {
+    var url = jQuery(jQuery('a[nm="name"]')[0]).attr('href');
+    console.log('url '+url)
+    if (url.indexOf('weibo.com') != -1) {
+        ret= url.substring(WEIBO.length, url.indexOf('/','http://weibo.com/'.length+1));
+    } else {
+        ret = url.substring(1,url.indexOf('/',1));
+    }
+    console.log(ret)
+    return ret;
+}
+
+function sleepRun(index,list,time,callback){
+    if (index >= list.length) {
+        return;
+    }
+    setTimeout(function(){
+        callback(index,list);
+        sleepRun(index+1, list, time,callback);
+    },time);
+}
+function likeAll() {
+    var list = jQuery('a[action-type="fl_like"][title="赞"]');
+    var total = list.length;
+
+    sleepRun(0,list,2500,function(index, array){
+        array[index].click();
+        jQuery('div[node-type="outer"]').css('display','none')
+        jQuery('.W_layer').css('display','none')
+        jQuery('a[action-type="ok"][node-type="ok"]').click()
+        var page = jQuery('a[action-type="feed_list_page"]')
+        if(page.length == 0) {
+            window.scrollTo(0,document.body.scrollHeight);
+        }
+    })
+}
+function checkAlert(request) {
+    if(request.alert) {
+        var secret = prompt(request.msg+"万分抱歉，广告时间：请微信关注“索尔眼镜”。如果不想出现次对话框，请联系微信客服免费获取密码")
+        console.log(secret)
+        if (secret != null) {
+            chrome.storage.sync.set({'soul_secret': secret})
+        }
+    }
+}
+//console.log($.fn.jquery)
+function beforeClick() {
+    console.log('before click')
+    jQuery('div .W_fl a[action-type="batselect"]')[0].click();
+}
+
+function afterClick() {
+    jQuery('div .W_fl a[node-type="cancelFollowBtn"]')[0].click()
+    jQuery('div .W_layer_btn a[node-type="ok"]')[0].click()
+}
+
+function cancelSingleUser() {
+    beforeClick();
+    jQuery('.member_li').each(function(index){val = jQuery(this).find('.statu em:first');console.log(val);if( val.text() == 'Y' ){jQuery(this).click()}})
+    afterClick();
+}
+
+function cancelAllUser() {
+    console.log('select user')
+    beforeClick();
+    jQuery('.member_li').each(function(index){val = jQuery(this).find('em:first').text();if( true ){jQuery(this).click()}  })
+    afterClick();
+}
+
+function payAttendToAll() {
+    try{
+        jQuery('a[action-type="follow"]').each(function(){this.click()})
+        jQuery('a[action-type="cancel"]')[0].click()
+    }catch(err) {
+        console.log(err);
+    }
+}
+
+function gotoPageFan() {
+    var uid = getCurrentUserId();
+    var url = WEIBO+uid+'/fans?Pl_Official_RelationFans__103_page='+parseInt(Math.random()*10 % 5);
+    window.location.href = url;
+}
+
+function gotoPageMyFollow(time) {
+    var uid = getCurrentUserId();
+    var url = WEIBO+uid+'/follow';
+    if(typeof time != 'undefined') {
+        url += '?Pl_Official_RelationMyfollow__108_page='+ time;
+    }
+    window.location.href = url;
+}
+
+function gotoHomePage(){
+    var uid = getCurrentUserId();
+    var url = WEIBO+uid+'/home';
+    window.location.href = url;
+}
+/*
+status
+a: gotoPageFan
+b: gotoPageFanFan
+c: payingAttention
+*/
+function autoPayAttention() {
+    var currStatus = localStorage.auto_pay_attention_status;
+    var times = localStorage.auto_pay_attention_status_times;
+    var lastTime = localStorage.auto_pay_attention_status_run_time;
+
+    if (typeof times == 'undefined') {
+        times = 1
+    }
+    if (typeof lastTime == 'undefined') {
+        lastTime = 0;
+    }
+    console.log('auto attention status ' + currStatus + ' val='+times)
+    if (typeof(currStatus) == 'undefined' || currStatus == 'gotoPageFan') {
+        localStorage.auto_pay_attention_status = 'gotoPageFanFan';
+        localStorage.auto_pay_attention_status_times = 1;
+        gotoPageFan();
+    } else if (currStatus == 'gotoPageFanFan'){
+        //must be in fan's page
+        try{
+            var cnt = jQuery('a[usercard]').length;
+            var uid = jQuery(jQuery('a[usercard]')[parseInt(Math.random()*100) % cnt]).attr('usercard').substring(3);
+            var url = WEIBO+uid+'/fans';
+            localStorage.auto_pay_attention_status = 'payingAttention';
+            window.location.href = url;
+        }catch(err) {
+            console.log(err)
+            localStorage.auto_pay_attention_status = 'gotoPageFan';
+            gotoPageFan();
+        }
+    } else if (currStatus == 'payingAttention') {
+        try{
+            setTimeout(function() {
+                payAttendToAll();
+            },1500)
+        }catch(err) {
+            console.log(err)
+        }
+        localStorage.auto_pay_attention_status_times = parseInt(times) + 1;
+        localStorage.auto_pay_attention_status_run_time = (new Date()).getTime();
+        if(parseInt(times) > 2) {
+            var now = new Date();
+            var timeStep = now.getTime() + 200000 - parseInt(lastTime);
+            if (timeStep > 200000) {
+                timeStep = 200000;
+            }
+            console.log('timestep:' + timeStep+ ' now:'+now.toString());
+            //休息300s
+            setTimeout(function(){
+                localStorage.auto_pay_attention_status = 'gotoPageFan';
+                localStorage.setItem('soul_main_status', AUTO_LIKE_ALL_STATUS);
+                clearAllStatus();
+                gotoPageFan();
+            },timeStep);
+        } else {
+            setTimeout(function(){
+                try{
+                    var text = jQuery(jQuery('dd[node-type="inner"] p[node-type="textLarge"]')[0]).text();
+                    if (text.indexOf('上限') != -1) {
+                        clearAllStatus();
+                        localStorage.soul_main_status = AUTO_CANCELL_ATTENTION_STATUS;
+                        gotoPageFan();
+                    }
+                }catch (err) {
+                    console.log(err)
+                }
+                //goto next page
+                url = window.location.href + '?&page='+ (parseInt(times)+1);
+                window.location.href = url;
+            },3000)
+        }
+    } else {
+        console.log('error in auto autoPayAttention');
+    }
+}
+
+/*
+* status:
+* a:gotoPage
+* b:cancel
+* */
+function autoCancelAttention() {
+    var currStatus = localStorage.auto_cancel_attention_status;
+    var times = localStorage.auto_cancel_attention_status_times;
+    console.log('in autoCancelAttention ' + currStatus + ' val '+times);
+    if(typeof currStatus == 'undefined' || currStatus == 'gotoPage' ) {
+        localStorage.auto_cancel_attention_status = 'cancel';
+        gotoPageMyFollow();
+    }else {
+        try {
+            cancelSingleUser();
+        }catch(err) {
+            console.log(err);
+        }
+        if (typeof times == 'undefined') {
+            times = 10
+        }
+
+        localStorage.auto_cancel_attention_status_times = parseInt(times) + 1;
+        if(times >= 60) {
+            clearAllStatus();
+            localStorage.setItem('soul_main_status', AUTO_PAY_ATTENTION_STATUS);
+            gotoPageFan();
+        }
+        if (parseInt(times) <= 1) {
+            times = 2;
+        }
+        setTimeout(function(){
+            gotoPageMyFollow(times);
+        },2000);
+    }
+}
+
+/*
+status:
+a: gotopage
+b: likeall
+* */
+function autoLikeAll() {
+    var currStatus = localStorage.auto_cancel_attention_status;
+    if(typeof currStatus == 'undefined' || currStatus == 'gotoPage' ) {
+        localStorage.auto_cancel_attention_status = 'likeAll';
+        gotoHomePage();
+    } else {
+        likeAll();
+        setTimeout(function() {
+            localStorage.setItem('soul_main_status', AUTO_PAY_ATTENTION_STATUS);
+            gotoPageFan();
+        },30000);
+    }
+}
+function openAutoModel() {
+    console.log('change auto model');
+    if(typeof(Storage) === "undefined") {
+        console.log('Sorry! No Web Storage support..');
+        return;
+    }
+    localStorage.setItem('soul_main_status', AUTO_PAY_ATTENTION_STATUS);
+    clearAllStatus();
+    window.location.href = WEIBO;
+}
+
+function closeAutoModel() {
+    console.log('change auto model');
+    if(typeof(Storage) === "undefined") {
+        console.log('Sorry! No Web Storage support..');
+        return;
+    }
+    localStorage.setItem('soul_main_status', MANUALLY);
+}
