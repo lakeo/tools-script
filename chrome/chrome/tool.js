d21781c58fbb2f6daa0210e939abe62ef6e381e3
lanxiaolu <blue.xiaolu@gmail.com>
2015-11-18 23:32:59 +0800

    fix

--- chrome/chrome/tool.js
+++ chrome/chrome/tool.js
@@ -7,46 +7,46 @@ var AUTO_LIKE_ALL_STATUS = 'auto_like_all_status';
 
 function setCookie(c_name,value)
 {
-    //var exdate=new Date()
-    //exdate.setDate(exdate.getDate()+1)
-    //document.cookie=c_name+ "=" +escape(value)+ ";expires="+exdate.toGMTString();
-    localStorage.setItem(c_name,value)
+    var exdate=new Date()
+    exdate.setDate(exdate.getDate()+1)
+    document.cookie=c_name+ "=" +escape(value)+ ";expires="+ exdate.toGMTString()+ ";domain=.weibo.com;path=/";
+    //localStorage.setItem(c_name,value)
 }
 
 function getCookie(c_name)
 {
-    return localStorage.getItem(c_name);
-    //if (document.cookie.length>0)
-    //{
-    //    c_start=document.cookie.indexOf(c_name + "=")
-    //    if (c_start!=-1)
-    //    {
-    //        c_start=c_start + c_name.length+1
-    //        c_end=document.cookie.indexOf(";",c_start)
-    //        if (c_end==-1) c_end=document.cookie.length
-    //        return unescape(document.cookie.substring(c_start,c_end))
-    //    }
-    //}
-    //return null;
+    //return localStorage.getItem(c_name);
+    if (document.cookie.length>0)
+    {
+        c_start=document.cookie.indexOf(c_name + "=")
+        if (c_start!=-1)
+        {
+            c_start=c_start + c_name.length+1
+            c_end=document.cookie.indexOf(";",c_start)
+            if (c_end==-1) c_end=document.cookie.length
+            return unescape(document.cookie.substring(c_start,c_end))
+        }
+    }
+    return null;
 }
 
 function deleteCookie( name ) {
-    //document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    localStorage.removeItem(name)
+    document.cookie = name + '=; domain=.weibo.com;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    //localStorage.removeItem(name)
 }
 
 function clearAllStatus() {
-    localStorage.removeItem('auto_pay_attention_status');
-    localStorage.removeItem('auto_pay_attention_status_times');
-    localStorage.removeItem('auto_cancel_attention_status');
-    localStorage.removeItem('auto_cancel_attention_status_times');
-    localStorage.removeItem('auto_cancel_attention_status')
+    //localStorage.removeItem('auto_pay_attention_status');
+    //localStorage.removeItem('auto_pay_attention_status_times');
+    //localStorage.removeItem('auto_cancel_attention_status');
+    //localStorage.removeItem('auto_cancel_attention_status_times');
+    //localStorage.removeItem('auto_cancel_attention_status')
 
-    //deleteCookie('auto_pay_attention_status');
-    //deleteCookie('auto_pay_attention_status_times');
-    //deleteCookie('auto_cancel_attention_status');
-    //deleteCookie('auto_cancel_attention_status_times');
-    //deleteCookie('auto_cancel_attention_status');
+    deleteCookie('auto_pay_attention_status');
+    deleteCookie('auto_pay_attention_status_times');
+    deleteCookie('auto_cancel_attention_status');
+    deleteCookie('auto_cancel_attention_status_times');
+    deleteCookie('auto_cancel_attention_status');
 }
 
 function getCurrentUserId() {
