805170ad62516af55b083dd616fe330ac638358a
lanxiaolu <blue.xiaolu@gmail.com>
2015-11-23 21:03:58 +0800

    fix

--- chrome/chrome/tool.js
+++ chrome/chrome/tool.js
@@ -310,7 +310,7 @@ function autoCancelAttention() {
     console.log('in autoCancelAttention ' + currStatus + ' val '+times);
     if(typeof currStatus == 'undefined' || currStatus == 'gotoPage' || currStatus == null) {
         setCookie('auto_cancel_attention_status','cancel');
-        gotoPageMyFollow(78);
+        gotoPageMyFollow(50);
     } else if (currStatus == 'cancelNotVipUser') {
         if (times >= 40) {
             clearAllStatus();
