c3dd5b011695f6d21ada8fae90a00b2b537dfa0f
lanxiaolu <blue.xiaolu@gmail.com>
2015-12-05 15:17:37 +0800

    fix

--- chrome/chrome/eventPage.js
+++ chrome/chrome/eventPage.js
@@ -1,5 +1,6 @@
 console.log('run in event page:' + (new Date()).toString())
 setTimeout(function(){
     console.log('auto refresh page:' + (new Date()).toString());
-    window.location.href='http://weibo.com/';
+    //window.location.href='http://weibo.com/';
+    window.open('http://www.weibo.com','blank')
 },10*60*1000);
