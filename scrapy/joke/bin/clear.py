2a4ecc294ac3072a1e42980a5d4140be16719c5c
lanxiaolu <blue.xiaolu@gmail.com>
2016-02-13 18:30:37 +0800

    add clear shell

---
+++ scrapy/joke/bin/clear.py
@@ -0,0 +1,28 @@
+import SimpleDB
+import hashlib
+
+db =  SimpleDB.MySQLdb(
+                host='localhost',
+                port = 3306,
+                user='joke',
+                passwd='joke',
+                db ='joke',
+                use_unicode=True,
+                charset='utf8')
+
+
+def getKey(item):
+        return hashlib.md5(item['content'].encode('unicode_escape').join(item['images'])).hexdigest()
+
+sql = 'select id,content,images from joke'
+rows = db.query_dict(sql)()
+jokes = []
+
+for r in rows:
+    jokes.append(r)
+
+update = 'update joke set md5code = %s where id = %s'
+
+for j in jokes:
+    key = getKey(j)
+    db.update(update,key,j['id'])
